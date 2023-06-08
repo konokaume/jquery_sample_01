84b07c6368af1f342f4d1a91de83728bb7ece3ca
konokaume <131327233+konokaume@users.noreply.github.com>
2023-06-08 12:07:09 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// buttonSet1
+$( function() {
+
+  $( '#buttonHeader' ).click( function() {
+    $( 'h1' ).text( 'Hello jQuery!' );
+  });
+
+  $( '#buttonArticle' ).click( function() {
+    $( 'article' ).css({
+      'color': 'white',
+      'background-color': '#fb3'
+    });
+  });
+
+  $( '#buttonFooter' ).click( function() {
+    $( 'footer' ).html('<p><a href="https://www.google.co.jp">Google</a></p>' );
+    $( 'footer a' ).css( 'color','#fc3' );
+  });
+
+});
+
+// buttonSet2
+$( function() {
+
+  $( '#buttonNormal' ).click( function() {
+    $('main').css( 'flex-direction','row' );
+  });
+
+  $( '#buttonReverse' ).click( function() {
+    $('main').css( 'flex-direction','row-reverse' );
+  });
+
+  $( '#buttonVertical' ).click( function() {
+    $('main').css( 'flex-direction','column' );
+  });
+
+
+
+});
